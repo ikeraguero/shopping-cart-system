742bb0308193f9b42e2a212a4c54c7dad1183f66
ikeraguero <ikerpires407@gmail.com>
2024-10-27 21:13:31 -0300

    Add TODOs and Comment Out Code to Improve

--- src/com/shoppingcart/stock/Stock.java
+++ src/com/shoppingcart/stock/Stock.java
@@ -7,27 +7,21 @@ import java.util.*;
 public class Stock {
     private static final HashMap<String, Integer> productQuantity = new HashMap<>();
     private static final List<Product> stock = new LinkedList<>();
-    private static final List<Product> groceriesList = new LinkedList<>();
-    private static final List<Product> electronicsList = new LinkedList<>();
-    private static final List<Product> clothingList = new LinkedList<>();
-    private static final List<List<Product>> listLists = new LinkedList<>(Arrays.asList(groceriesList, electronicsList, clothingList));
+//    private static final List<Product> groceriesList = new LinkedList<>();
+//    private static final List<Product> electronicsList = new LinkedList<>();
+//    private static final List<Product> clothingList = new LinkedList<>();
+//    private static final List<List<Product>> listLists = new LinkedList<>(Arrays.asList(groceriesList, electronicsList, clothingList));
 
     public Stock() {
     }
 
     public static void addItemStock(Product item) {
-        stock.add(item);
-        switch (item.getCategory()) {
-            case "groceries":
-                groceriesList.add(item);
-                break;
-            case "electronics":
-                electronicsList.add(item);
-                break;
-            case "clothing":
-                clothingList.add(item);
-                break;
+        if(stock.stream().anyMatch(o->o.getName().equals(item.getName()))) // streams and lambda to check if there's already an object with the same name value
+        {
+            // TODO: Code to update the total product amount
+            return;
         }
+        stock.add(item);
         calculateQuantity();
     }
 
@@ -51,38 +45,11 @@ public class Stock {
         }
         switch (option) {
             case 1:
-                switch (category) {
-                   case "groceries":
-                        groceriesList.remove(getFirstItem(name));
-                        break;
-                    case "electronics":
-                        electronicsList.remove(getFirstItem(name));
-                        break;
-                    case "clothing":
-                        clothingList.remove(getFirstItem(name));
-                        break;
-                }
                 stock.remove(getFirstItem(name));
+                // TODO: Code to remove only 1 unit
                 break;
             case 2:
-                Iterator<Product> iterator = stock.iterator();
-                while(iterator.hasNext()) {
-                    Product product = iterator.next();
-                    if(product.getName().equals(name)) {
-                        iterator.remove();
-                    }
-                }
-                switch (category) {
-                    case "groceries":
-                        removeSingleItem(groceriesList, name);
-                        break;
-                    case "electronics":
-                        removeSingleItem(electronicsList, name);
-                        break;
-                    case "clothing":
-                        removeSingleItem(clothingList, name);
-                        break;
-                }
+                stock.remove(getFirstItem(name));
                 break;
         }
         calculateQuantity();
@@ -107,43 +74,43 @@ public class Stock {
     }
 
     private static void calculateQuantity() {
-        for (List<Product> list : Stock.listLists) {
-            if (list.isEmpty()) continue;
-            HashMap<String, Product> productMap = new HashMap<>();
-            for (Product item : list) {
-                if (!productMap.containsKey(item.getName())) {
-                    int productCount = 0;
-                    productMap.put(item.getName(), item);
-                    for (Product itemList : list) {
-                        if (itemList.getName().equals(item.getName())) {
-                            productCount++;
-                        }
-                    }
-                    productQuantity.put(item.getName(), productCount);
-                }
-            }
-        }
+//        for (List<Product> list : Stock.listLists) {
+//            if (list.isEmpty()) continue;
+//            HashMap<String, Product> productMap = new HashMap<>();
+//            for (Product item : list) {
+//                if (!productMap.containsKey(item.getName())) {
+//                    int productCount = 0;
+//                    productMap.put(item.getName(), item);
+//                    for (Product itemList : list) {
+//                        if (itemList.getName().equals(item.getName())) {
+//                            productCount++;
+//                        }
+//                    }
+//                    productQuantity.put(item.getName(), productCount);
+//                }
+//            }
+//        }
     }
 
     private static void printSortedStock() {
         System.out.println("\n============== Stock =================");
-        for (List<Product> list: Stock.listLists) {
-            if(list.isEmpty()) continue;
-            System.out.println();
-            System.out.println(list.getFirst().getCategory().toUpperCase());
-            HashMap<String, Product> productMap = new HashMap<>();
-            for(Product item : list) {
-                    if(!productMap.containsKey(item.getName())) {
-                        int productCount = 0;
-                        productMap.put(item.getName(), item);
-                        for(Product itemList : list) {
-                            if(itemList.getName().equals(item.getName())) {
-                                productCount++;
-                            }
-                        }
-                        System.out.printf("%-20sR$%.2f (%d %s)\n", item.getName(), item.getFinalPrice(), productCount, productCount > 1 ? "units" : "unit");
-                    }
-            }
+//        for (List<Product> list: Stock.listLists) {
+//            if(list.isEmpty()) continue;
+//            System.out.println();
+//            System.out.println(list.getFirst().getCategory().toUpperCase());
+//            HashMap<String, Product> productMap = new HashMap<>();
+//            for(Product item : list) {
+//                    if(!productMap.containsKey(item.getName())) {
+//                        int productCount = 0;
+//                        productMap.put(item.getName(), item);
+//                        for(Product itemList : list) {
+//                            if(itemList.getName().equals(item.getName())) {
+//                                productCount++;
+//                            }
+//                        }
+//                        System.out.printf("%-20sR$%.2f (%d %s)\n", item.getName(), item.getFinalPrice(), productCount, productCount > 1 ? "units" : "unit");
+//                    }
+//            }
 //            for(Product item : list) {
 //                if(productMap.containsKey(item.getName())) continue;
 //                productMap.put(item.getName(), item);
@@ -151,8 +118,7 @@ public class Stock {
 //
 //            }
         }
-        System.out.println("\n======================================");
-    }
+//        System.out.println("\n======================================");
 
     public static void searchItem(String itemName){
         for(Product item : stock) {
@@ -170,16 +136,16 @@ public class Stock {
         sortType = (sortType == 0 ? 1 : sortType);
         switch (sortType) {
             case 1:
-                electronicsList.sort(Comparator.comparing(Product::getName));
-                groceriesList.sort(Comparator.comparing(Product::getName));
-                clothingList.sort(Comparator.comparing(Product::getName));
-                printSortedStock();
+//                electronicsList.sort(Comparator.comparing(Product::getName));
+//                groceriesList.sort(Comparator.comparing(Product::getName));
+//                clothingList.sort(Comparator.comparing(Product::getName));
+//                printSortedStock();
                 break;
             case 2:
-                electronicsList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
-                groceriesList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
-                clothingList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
-                printSortedStock();
+//                electronicsList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
+//                groceriesList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
+//                clothingList.sort(Comparator.comparing(Product::getFinalPrice).reversed());
+//                printSortedStock();
                 break;
 
         }
